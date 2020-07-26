67d568c23a15e91223bf8b7338cc36331c2e9687
Mayur Awaghade <awaghade.mayur@gmail.com>
2020-07-26 01:18:56 -0700

    Add a docker init script
    The docker init script allows you to override the default entrypoint
    into a docker container to set up a specified user inside the container.
    This helps to match your user inside and outside the container

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	cp docker-init.sh /usr/local/bin/
+
+uninstall:
+	rm /usr/local/bin/docker-init.sh
---
+++ docker-init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+RUN_CMD="$@"
+useradd ${RUN_USER} -u ${RUN_UID} -s ${RUN_SHELL}
+if [ -z "${RUN_CMD}" ]; then
+  su ${RUN_USER}
+else
+  su -p ${RUN_USER} -c "${RUN_CMD}"
+fi
